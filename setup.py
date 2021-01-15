92183d2c70ee075f171adce8d92f6b3c1ce196fd
chris <christopher.m9876@gmail.com>
2021-01-16 01:45:33 +0530

    renamed source file and updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="package_passgen",
-    version="0.0.1",
+    version="0.0.2",
     author="Chris",
     author_email="christopher.m9876@gmail.com",
     description="A package to generate strong random passwords",
